82920d3c65a38ebc61eb34b45728a863e044d527
Valeriy <valerik.shadowman@gmail.com>
2023-10-13 01:22:11 +0800

    work in models

--- signs/admin.py
+++ signs/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import SignBase
 
 # Register your models here.
+
+@admin.register (SignBase)
+class AdminSignBase(admin.ModelAdmin):
+    list_display = ('subject_name', 'public_key')
--- signs/models.py
+++ signs/models.py
@@ -2,3 +2,5 @@ from django.db import models
 
 class SignBase (models.Model):
     public_key = models.FileField()
+    private_key = models.FileField()
+    subject_name = models.CharField(max_length=100)
