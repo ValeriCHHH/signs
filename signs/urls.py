9a267ddf3efb15383372d31a34b1325436486f71
tomas <you@example.com>
2023-10-12 13:48:31 +0800

    redirect_pages

--- signs/urls.py
+++ signs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.signs),
+    path('', views.home),
+    path('signs/', views.sign),
 ]
--- signs/views.py
+++ signs/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def signs (request):
+def home (request):
+    return render(request, 'signs/home.html')
+
+def sign (request):
     return render(request, 'signs/signs.html')
