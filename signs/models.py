5408fc6112b335feed4634499654e64871597b73
Valeriy <valerik.shadowman@gmail.com>
2023-10-13 23:24:03 +0800

    3

--- signs/models.py
+++ signs/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+import fsb795
 
 class SignBase (models.Model):
     public_key = models.FileField()
     private_key = models.FileField()
+    final = fsb795.Certificate(public_key)
     subject_name = models.CharField(max_length=100)
+    date_final = models.DateTimeField(final.validityCert())
+    
