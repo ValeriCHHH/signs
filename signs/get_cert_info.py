d111c3a293bd0edfdfe037395efa5cbffe530139
Valeriy <valerik.shadowman@gmail.com>
2023-10-17 02:12:46 +0800

    def in progress

--- signs/get_cert_info.py
+++ signs/get_cert_info.py
@@ -2,32 +2,34 @@ import fsb795
 import zipfile
 import os
 import re
-import pathlib
 
 
 
-pattern_cer = r"\w+[- ]*\w*[ -.\s]?\w*[ -.\s]?\w*[ -.\s]?\w*[ -.\s]?\w*[ -.\s]?\w*[ -.\s]?\w*.cer"
+pattern_cer = r"\w+[- ]?\w+[ -\.]?\w+[ -\.]?\w+[ -\.]?\w+[ -\.]?\w+[ -\.]?\w+[ -\.]?\w+\.cer"
 pattern_zip = r"\w+[- ]*\w*[ -.\s]?\w*[ -.\s]?\w*.zip"
 
-print (os.getcwd())
 sequence_zip = str(os.listdir('./media'))
-
 result_search = re.search(pattern_zip, sequence_zip).group()
-
 path_zip = f'./media/{result_search}'
-
-zipfile.is_zipfile(str(path_zip))
+zipfile.ZipFile(str(path_zip))
 z = zipfile.ZipFile(path_zip, 'r')
 sequence_cer = str(z.namelist())
-#print(sequence_cer)
-
 result_search_cert = re.search(pattern_cer, sequence_cer).group()
-
+path_zip = f'./media/{result_search}'
+zipfile.ZipFile(str(path_zip))
+z = zipfile.ZipFile(path_zip, 'r')
 
 f = open(path_zip,'rb')
-zfile = zipfile.ZipFile(f)
+zfile = zipfile.ZipFile(f, 'r')
 for zc in zfile.namelist():
-    zfile.extract(result_search_cert,'./media', pwd = b'12345')
+    zfile.extract(result_search_cert,'./media')
+    sequence_cer_unzip = str(os.listdir('./media'))
+    result_search_unzip_cert = re.search(pattern_cer, sequence_cer_unzip).group()
+
+cert = f'./media/{result_search_unzip_cert}'
+
+
+
 
 
 def get_cert_info (cert):
@@ -39,13 +41,15 @@ def get_cert_info (cert):
         lst = {key:sub[key]}
     for key in iss.keys():
         dst = {key:iss[key]}
-#    print(lst.get('CN'), valid['not_after'], dst)
     sub_name = lst.get('CN')
     dst_name = dst.get('CN')
     end_date = valid['not_after']
     print (f'{sub_name} \n{dst_name} \n{end_date}')
+    
 
-get_cert_info ('/home/tomas/Рабочий стол/Averkina_pers/Аверкина Олеся Александровна.cer')
-print (sequence_zip)
-print (path_zip)
-print (result_search_cert)
+get_cert_info (cert)
+#print (sequence_zip)
+#print (path_zip)
+#print (result_search_cert)
+#print(sequence_cer_unzip)
+#print(result_search_unzip_cert)
